7b5cc3f8bddb0ba3fd9f0001900fe7a47526b3a4
rafiulIslamRobin <89656203+rafiulIslamRobin@users.noreply.github.com>
2021-09-18 02:17:16 -0700

    UVa problem NO: 11727 solution in c

---
+++ middle number others way.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int t,i=0;
+    scanf("%d", &t);
+
+    for(i=1; i<=t; i++)
+    {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        if(a > b)
+        {
+
+         if(c > a)
+
+                printf("Case %d: %d\n", i, a);
+
+            else if(b > c)
+
+                printf("Case %d: %d\n", i, b);
+
+                else
+                    printf("Case %d: %d\n", i, c);
+        }
+        else
+        {
+            if(b < c)
+                printf("Case %d: %d\n", i, b);
+
+            else if(a > c)
+                printf("Case %d: %d\n", i, a);
+
+            else
+                printf("Case %d: %d\n", i, c);
+        }
+
+    }
+
+    return 0;
+}
