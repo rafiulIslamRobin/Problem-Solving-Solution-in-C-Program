82a9a66b6674544c0d97f808238a8bb7ed3ed291
rafiulIslamRobin <89656203+rafiulIslamRobin@users.noreply.github.com>
2021-09-18 01:25:57 -0700

    UVa Problem NO:11727 Solution

---
+++ middle number in c.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int t,i=0;
+    scanf("%d", &t);
+
+    for(i=1; i<=t; i++)
+    {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        if(a<c && b<a || a<b && a>c)
+            printf("Case %d: %d\n", i, a);
+
+        else if(a<b && b<c || a>c && b>c)
+            printf("Case %d: %d\n", i, b);
+
+        else if(a<c && b>c || a>c && b<c)
+            printf("Case %d: %d\n", i, c);
+    }
+
+    return 0;
+}
